3e8b5c6103bc9013c3d502a9cdfb7c664e9ea22f
Julio Peña <juliopa54@gmail.com>
2023-08-22 19:10:30 -0400

    added a very basic routing system

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,20 +1,30 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import './App.css'
 import { Home } from '../Home'
+import { Account } from '../Account'
 import { NotFound } from '../NotFound'
 import { Orders } from '../Orders'
 import { Order } from '../Order'
 import { SignIn } from '../SignIn'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/account', element: <Account /> },
+    { path: '/orders', element: <Orders /> },
+    { path: '/order', element: <Order /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+  ])
 
+  return routes
+}
+
+const App = () => {
   return (
-    <>  
-      <Home />
-      <NotFound />
-      <Orders />
-      <Order />
-      <SignIn />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 

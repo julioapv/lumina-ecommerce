31ab9625d2c087a97e8a465c8a150cba6cfe133d
Julio Peña <juliopa54@gmail.com>
2023-08-31 16:04:48 -0400

    used a fake store API, though I will change the way I didt it later for a more professional one

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,25 @@
+import { useState, useEffect } from 'react';
 import { Layout } from '../../components/Layout'
 import { Card } from '../../components/Card'
 
 const Home = () => {
+    const [products, setProducts] = useState(null) 
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+    }, [])
+
     return (
         <Layout>
             Home
-            <div className='flex gap-3'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <div className='grid gap-3 grid-cols-4 w-full max-w-screen-lg'>
+            {
+                products?.map((item) => (
+                    <Card key={item.id} data={item} />
+                ))
+            }
             </div>
         </Layout>
     );
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,17 @@
-const Card = () => {
+const Card = (data) => {
     return (
-        <div className="bg-white cursor-pointer w-56 h-80 rounded border-black border-2 hover:shadow-custom">
+        <div className="bg-white cursor-pointer w-56 h-96 rounded border-black border-2 hover:shadow-custom">
             <figure className='relative mb-4 w-full h-4/5 p-4 border-black border-b-2'>
-                <img className="w-full h-full object-cover rounded-sm border-black border-[1px]" src="https://www.byrdie.com/thmb/ggjyFcBnEs8-7Sd4nLLBToWfUwY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/sipsip-abdb129dd0044b548110804f49de246f.jpg" alt="dress" />
+                <img className="w-full h-full object-cover rounded-sm border-black border-[1px]" src={data.data.images[0]} alt="dress" />
                 <button className="absolute top-0 right-0 flex justify-center items-center bg-purple-400 w-8 h-8 rounded-bl-lg border-black border-b-[1px]" role="button">+</button>
             </figure>
 
             <div className='flex justify-between px-2'>
                 <div className='flex flex-col'>
-                    <span className='text-base font-light'>Dress</span>
-                    <span className='text-xs font-light'>Clothing</span>
+                    <span className='text-base font-light'>{data.data.title}</span>
+                    <span className='text-xs font-light'>{data.data.category.name}</span>
                 </div>
-                <span className='text-lg font-bold'>$250</span>
+                <span className='text-lg font-bold'>{`$${data.data.price}`}</span>
             </div>
         </div>
     )

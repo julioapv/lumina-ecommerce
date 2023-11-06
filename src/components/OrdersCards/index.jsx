884fcd5d26a0081a0a916634dde1c4f205a38ccf
Julio Peña <juliopa54@gmail.com>
2023-11-06 17:31:02 -0400

    worked on the components used in the orders page

--- src/components/OrdersCards/index.jsx
+++ src/components/OrdersCards/index.jsx
@@ -1,25 +1,30 @@
-import { AiOutlineClose, AiOutlineShoppingCart, AiOutlineCalendar, AiOutlineRight  } from "react-icons/ai";
+import {AiOutlineShoppingCart, AiOutlineCalendar, AiOutlineRight  } from "react-icons/ai";
 
 const OrdersCard = (props) => {
     const { totalPrice, totalProducts, date} = props
 
     return (
-        <div className="flex justify-between items-center mb-3 border-black  px-1 rounded border-[1px]">
-            <AiOutlineClose />
-            <p className="flex p-2 gap-3 text-lg">
-                <span className="flex items-center gap-1">
-                    <AiOutlineShoppingCart className="text-xl"/>
-                    <span className="">{totalProducts}</span>
-                </span>
-                <span className="flex items-center gap-1">
-                    <span className=""><span className="text-xl">$</span>{totalPrice}</span>
-                </span>
-                <span className="flex items-center gap-1">
-                    <AiOutlineCalendar className="text-xl"/>
-                    <span className="">{date}</span>
-                </span>
-            </p>
-            <AiOutlineRight />
+        <div className="flex items-center justify-center mb-5 cursor-pointer w-full">
+            <div className="border-black p-4 rounded border-[1px] w-80">
+                <div className="flex justify-between w-full">
+                    <span className="flex flex-col justify-center  items-start gap-1 text-xl">
+                        <span className="flex gap-1 justify-start items-center">
+                            <AiOutlineCalendar className="font-bold"/>
+                            {date}
+                        </span>
+                        <span className="flex gap-1 justify- items-center">
+                            <AiOutlineShoppingCart className="font-bold"/>
+                            {totalProducts}
+                        </span>
+                    </span>
+
+                    <span className="flex items-center gap-1 text-2xl font-bold">
+                            <span>$</span>
+                            {totalPrice}
+                            <AiOutlineRight className="text-purple-500 font-extrabold" />
+                    </span>
+                </div>
+            </div>
         </div>
 
     )
